f6e447fec11fc636fe8fffe20f756f7e92420ff1
krishna3571 <krishna9723577963@gmail.com>
2023-10-16 19:48:47 +0530

    js

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './Layout'
+
+export default function Home() {
+    return (
+
+        <Layout>
+            <div>Home</div>
+        </Layout>
+
+
+    )
+}
