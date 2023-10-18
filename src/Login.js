68787f951a2f81eb4794c07b4d467de6cbd95cec
naitiksheladiya <sheladiyanaitik502@gmail.com>
2023-10-18 17:20:41 +0530

    asd

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,49 @@
 import React from 'react'
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 export default function Login() {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .min(2, "Mininum 2 characters")
+                .max(15, "Maximum 15 characters")
+                .required("Required!"),
+            email: Yup.string()
+                .email("Invalid email format")
+                .required("Required!"),
+
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+
     return (
-        <div>Login</div>
+        <>
+            <div class="container">
+                <div class="card">
+                    <h2>Login Form</h2>
+
+                    <label for="username">Username</label>
+                    <input type="text" id="username" placeholder="Enter your username" />
+
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" />
+
+                    <button type="submit">Login</button>
+
+                </div>
+            </div>
+
+
+        </>
+
     )
 }
--- src/Signin.js
+++ src/Signin.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from './Layout'
 
+
 export default function Signin() {
+
+
   return (
     <>
       <Layout>
-        <h1>Signin</h1>
+
       </Layout>
     </>
   )
