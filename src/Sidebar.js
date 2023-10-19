0c6f57e4a558e01c68ea7f40875f18991c18b757
naitiksheladiya <sheladiyanaitik502@gmail.com>
2023-10-19 19:58:16 +0530

    dasdds

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,219 +1,10 @@
-// import * as React from 'react';
-// import { styled, useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import MuiDrawer from '@mui/material/Drawer';
-// import MuiAppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
-// import List from '@mui/material/List';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Typography from '@mui/material/Typography';
-// import Divider from '@mui/material/Divider';
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
-// import CloseIcon from '@mui/icons-material/Close';
-// import './Sidebar.css'
 
-// const drawerWidth = 240;
-
-// const openedMixin = (theme) => ({
-//   width: drawerWidth,
-//   transition: theme.transitions.create('width', {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.enteringScreen,
-//   }),
-//   overflowX: 'hidden',
-// });
-
-// const closedMixin = (theme) => ({
-//   transition: theme.transitions.create('width', {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   overflowX: 'hidden',
-//   width: `calc(${theme.spacing(7)} + 1px)`,
-//   [theme.breakpoints.up('sm')]: {
-//     width: `calc(${theme.spacing(8)} + 1px)`,
-//   },
-// });
-
-// const DrawerHeader = styled('div')(({ theme }) => ({
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'flex-end',
-//   padding: theme.spacing(0, 1),
-//   // necessary for content to be below app bar
-//   ...theme.mixins.toolbar,
-// }));
-
-// const AppBar = styled(MuiAppBar, {
-//   shouldForwardProp: (prop) => prop !== 'open',
-// })(({ theme, open }) => ({
-//   zIndex: theme.zIndex.drawer + 1,
-//   transition: theme.transitions.create(['width', 'margin'], {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   ...(open && {
-//     marginLeft: drawerWidth,
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     transition: theme.transitions.create(['width', 'margin'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   }),
-// }));
-
-// const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-//   ({ theme, open }) => ({
-//     width: drawerWidth,
-//     flexShrink: 0,
-//     whiteSpace: 'nowrap',
-//     boxSizing: 'border-box',
-//     ...(open && {
-//       ...openedMixin(theme),
-//       '& .MuiDrawer-paper': openedMixin(theme),
-//     }),
-//     ...(!open && {
-//       ...closedMixin(theme),
-//       '& .MuiDrawer-paper': closedMixin(theme),
-//     }),
-//   }),
-// );
-
-// export default function MiniDrawer() {
-//   const theme = useTheme();
-//   const [open, setOpen] = React.useState(false);
-
-//   const handleDrawerOpen = () => {
-//     setOpen(true);
-//     document.getElementById('a').classList.add('bigMargin')
-//   };
-
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//     document.getElementById('a').classList.remove('bigMargin')
-//   };
-
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CssBaseline />
-//       <AppBar position="fixed" open={open}>
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             onClick={handleDrawerOpen}
-//             edge="start"
-//             sx={{
-//               marginRight: 5,
-//               ...(open && { display: 'none' }),
-//             }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" noWrap component="div">
-//             Business-Systeam
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer variant="permanent" open={open}>
-//         <DrawerHeader className='Side-bar' >
-//           <IconButton onClick={handleDrawerClose}>
-//             {theme.direction === 'rtl' ? <CloseIcon /> : <CloseIcon />}
-//           </IconButton>
-//         </DrawerHeader>
-//         <Divider />
-//         <List>
-//           {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-//             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-//               <ListItemButton
-//                 sx={{
-//                   minHeight: 48,
-//                   justifyContent: open ? 'initial' : 'center',
-//                   px: 2.5,
-//                 }}
-//               >
-//                 <ListItemIcon
-//                   sx={{
-//                     minWidth: 0,
-//                     mr: open ? 3 : 'auto',
-//                     justifyContent: 'center',
-//                   }}
-//                 >
-//                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//                 </ListItemIcon>
-//                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-//               </ListItemButton>
-//             </ListItem>
-//           ))} */}
-//           <ListItem key={'Create Bill'} disablePadding sx={{ display: 'block' }}>
-//             <ListItemButton
-//               sx={{
-//                 minHeight: 48,
-//                 justifyContent: open ? 'initial' : 'center',
-//                 px: 2.5,
-//               }}
-//             >
-//               <ListItemIcon
-//                 sx={{
-//                   minWidth: 0,
-//                   mr: open ? 3 : 'auto',
-//                   justifyContent: 'center',
-//                 }}
-//               >
-//                 {"Create-bill" % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={'Create Bill'} sx={{ opacity: open ? 1 : 0 }} />
-//             </ListItemButton>
-//           </ListItem>
-//         </List>
-//         <Divider />
-//       </Drawer>
-
-//     </Box>
-//   );
-// }
-
-
-
-// import React, { useState } from 'react';
-// import './Sidebar.css';
-
-// const Sidebar = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleSidebar = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <>
-//       <h1>af</h1>
-//       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-//         <button onClick={toggleSidebar}>Toggle Sidebar</button>
-//         <ul>
-//           <li>Menu Item 1</li>
-//           <li>Menu Item 2</li>
-//           <li>Menu Item 3</li>
-//           {/* Add more menu items as needed */}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Sidebar;
 
 
 
 import React, { useState } from 'react';
-import './Sidebar.css'; // Import your CSS file here
+import './Sidebar.css'; // Import your CSS file here 
+import { AiOutlinePlusSquare, AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
 
 const Dashboard = () => {
   const [isSidebarClosed, setSidebarClosed] = useState(false);
@@ -221,6 +12,13 @@ const Dashboard = () => {
 
   const toggleSidebar = () => {
     setSidebarClosed(!isSidebarClosed);
+    if (isSidebarClosed) {
+      document.getElementById('a').classList.add('Home')
+      document.getElementById('b').classList.remove('hk')
+    } else {
+      document.getElementById('a').classList.remove('Home')
+      document.getElementById('b').classList.add('hk')
+    }
   };
 
   const toggleDarkMode = () => {
@@ -242,21 +40,21 @@ const Dashboard = () => {
             </div>
           </div>
 
-          <i className={`bx bx-chevron-right toggle`} onClick={toggleSidebar}></i>
+          <i className={`bx bx-chevron-right toggle  asd`} onClick={toggleSidebar}>  <AiOutlineRight /></i>
         </header>
 
         <div className="menu-bar">
           <div className="menu">
             <li className="search-box" onClick={() => setSidebarClosed(false)}>
-              <i className='bx bx-search icon'></i>
+              <i className='bx bx-search icon'> < AiOutlineSearch /></i>
               <input type="text" placeholder="Search Games..." />
             </li>
 
             <ul className="menu-links menu-pp">
               <li className="nav-link">
                 <a href="#">
-                  <i className='bx bx-home-alt icon'></i>
-                  <span className="text nav-text">Dashboard</span>
+                  <i className='bx bx-bar-chart-alt-2 icon'>   <  AiOutlinePlusSquare />  </i>
+                  <span className="text nav-text">ADD ITEM</span>
                 </a>
               </li>
 
@@ -320,9 +118,7 @@ const Dashboard = () => {
         </div>
       </nav>
 
-      <section className="home">
-        <div className="text">Dashboard Sidebar</div>
-      </section>
+
     </div>
   );
 };
