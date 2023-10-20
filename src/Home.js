2fb2717a5260484e6e7e4a1a39a8b61f302530a1
krishna3571 <krishna9723577963@gmail.com>
2023-10-20 19:53:15 +0530

    all

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from './Layout'
 import Sidebar from './Sidebar'
 import './Sidebar.css'
+
+import './App.css';
 
 export default function Home() {
 
@@ -9,7 +11,34 @@ export default function Home() {
         console.log('first')
     }
 
+    const HandalClick = () => {
+
+    }
+
+    const [invoiceItems, setInvoiceItems] = useState([
+        { id: 1, description: 'Item 1', quantity: 2, price: 10 },
+        { id: 2, description: 'Item 2', quantity: 1, price: 20 },
+    ]);
+
+    const [newItem, setNewItem] = useState({ description: '', quantity: 1, price: 0 });
+
+    const addNewItem = () => {
+        setInvoiceItems([...invoiceItems, { ...newItem, id: invoiceItems.length + 1 }]);
+        setNewItem({ description: '', quantity: 1, price: 0 });
+    };
+
+    const removeItem = (id) => {
+        const updatedItems = invoiceItems.filter((item) => item.id !== id);
+        setInvoiceItems(updatedItems);
+    };
+
+    const calculateTotal = () => {
+        return invoiceItems.reduce((total, item) => total + item.quantity * item.price, 0);
+    };
+
     return (
+
+
 
         <>
             <Sidebar />
@@ -18,23 +47,72 @@ export default function Home() {
 
 
                 <div className='Home' id='a'>
-                    <div>Home</div>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
-                    <h1>ghjkl</h1>
+                    <h1>Invoice Billing System</h1>
+                    <h2 onClick={HandalClick}>Add New Item</h2>
+                    <div className="new-item-form">
+                        <input
+                            type="text"
+                            placeholder="Description"
+                            value={newItem.description}
+                            onChange={(e) => setNewItem({ ...newItem, description: e.target.value })}
+                        />
+                        <input
+                            type="number"
+                            placeholder="Quantity"
+                            value={newItem.quantity}
+                            onChange={(e) => setNewItem({ ...newItem, quantity: parseInt(e.target.value) })}
+                        />
+                        <input
+                            type="number"
+                            placeholder="Price"
+                            value={newItem.price}
+                            onChange={(e) => setNewItem({ ...newItem, price: parseFloat(e.target.value) })}
+                        />
+                        <button onClick={addNewItem}>Add Item</button>
+                    </div>
+
+
+                    <div className="App">
+
+                        <div className="invoice">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Description</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                        <th>Total</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {invoiceItems.map((item) => (
+                                        <tr key={item.id}>
+                                            <td>{item.description}</td>
+                                            <td>{item.quantity}</td>
+                                            <td>{item.price}</td>
+                                            <td>{item.quantity * item.price}</td>
+                                            <td>
+                                                <button onClick={() => removeItem(item.id)}>Remove</button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                        <div className="total">
+                            <h2>Total: {calculateTotal()}</h2>
+                        </div>
+
+
+                    </div>
+
+
+
+
+
                 </div>
             </div>
         </>
