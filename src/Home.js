44f465cdbe831afa26c7931a7ddddee3cb6c3e7b
krishna3571 <krishna9723577963@gmail.com>
2023-10-23 15:48:49 +0530

    add

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from 'react'
 import MUIDataTable from "mui-datatables";
 import createCache from '@emotion/cache';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useFormik } from 'formik';
 import { CacheProvider } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
@@ -16,6 +17,7 @@ const muiCache = createCache({
     key: 'mui-datatables',
     prepend: true
 });
+
 
 export default function Home() {
     const columns = [
@@ -101,13 +103,36 @@ export default function Home() {
         { id: 1, description: 'Item 1', quantity: 2, price: 10 },
         { id: 2, description: 'Item 2', quantity: 1, price: 20 },
     ]);
-    const [newItem, setNewItem] = useState({ description: '', quantity: 1, price: 0 });
+
+    const formik = useFormik({
+        initialValues: {
+            description: '',
+            quantity: 1,
+            price: 0,
+            id: Date.now()
+        },
+        onSubmit: (values, { resetForm }) => {
+            if (editItem) {
+                const updatedItems = invoiceItems.map((item) =>
+                    item.id === editItem.id ? { ...item, ...values } : item
+                );
+                setInvoiceItems(updatedItems);
+                setEditItem(null);
+            } else {
+                setInvoiceItems([...invoiceItems, { ...values }]);
+            }
+            resetForm();
+            setShowModal(false);
+        },
+    });
 
     const handleShowModal = () => {
+        formik.resetForm();
         setShowModal(true);
     };
 
     const handleCloseModal = () => {
+        formik.resetForm();
         setShowModal(false);
     };
 
@@ -120,29 +145,12 @@ export default function Home() {
         const itemToEdit = invoiceItems.find(item => item.id === id);
         setEditItem(itemToEdit);
         setShowModal(true);
-    };
-
-    const handleSaveEdit = () => {
-        const updatedItems = invoiceItems.map(item => {
-            if (item.id === editItem.id) {
-                return editItem;
-            }
-            return item;
+        formik.setValues({
+            description: itemToEdit.description,
+            quantity: itemToEdit.quantity,
+            price: itemToEdit.price,
         });
-        setInvoiceItems(updatedItems);
-        setShowModal(false);
     };
-
-    const handleCancelEdit = () => {
-        setShowModal(false);
-    };
-
-    const handleAddItem = () => {
-        setInvoiceItems([...invoiceItems, { ...newItem, id: Date.now() }]);
-        setNewItem({ description: '', quantity: 1, price: 0 });
-        setShowModal(false);
-    };
-
     const calculateTotal = () => {
         return invoiceItems.reduce((total, item) => total + item.quantity * item.price, 0);
     };
@@ -165,18 +173,15 @@ export default function Home() {
                             <Modal.Title>{editItem ? 'Edit Item' : 'Add New Item'}</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <Form>
+                            <Form onSubmit={formik.handleSubmit}>
                                 <Form.Group>
                                     <Form.Label>Description</Form.Label>
                                     <Form.Control
                                         type="text"
                                         placeholder="Description"
-                                        value={editItem ? editItem.description : newItem.description}
-                                        onChange={(e) =>
-                                            editItem
-                                                ? setEditItem({ ...editItem, description: e.target.value })
-                                                : setNewItem({ ...newItem, description: e.target.value })
-                                        }
+                                        name="description"
+                                        value={formik.values.description}
+                                        onChange={formik.handleChange}
                                     />
                                 </Form.Group>
                                 <Form.Group>
@@ -184,12 +189,9 @@ export default function Home() {
                                     <Form.Control
                                         type="number"
                                         placeholder="Quantity"
-                                        value={editItem ? editItem.quantity : newItem.quantity}
-                                        onChange={(e) =>
-                                            editItem
-                                                ? setEditItem({ ...editItem, quantity: parseInt(e.target.value) })
-                                                : setNewItem({ ...newItem, quantity: parseInt(e.target.value) })
-                                        }
+                                        name="quantity"
+                                        value={formik.values.quantity}
+                                        onChange={formik.handleChange}
                                     />
                                 </Form.Group>
                                 <Form.Group>
@@ -197,27 +199,19 @@ export default function Home() {
                                     <Form.Control
                                         type="number"
                                         placeholder="Price"
-                                        value={editItem ? editItem.price : newItem.price}
-                                        onChange={(e) =>
-                                            editItem
-                                                ? setEditItem({ ...editItem, price: parseFloat(e.target.value) })
-                                                : setNewItem({ ...newItem, price: parseFloat(e.target.value) })
-                                        }
+                                        name="price"
+                                        value={formik.values.price}
+                                        onChange={formik.handleChange}
                                     />
                                 </Form.Group>
+                                <Button variant="secondary" onClick={handleCloseModal}>
+                                    Cancel
+                                </Button>
+                                <Button variant="primary" type="submit">
+                                    {editItem ? 'Save Changes' : 'Add Item'}
+                                </Button>
                             </Form>
                         </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={editItem ? handleCancelEdit : handleCloseModal}>
-                                Cancel
-                            </Button>
-                            <Button
-                                variant="primary"
-                                onClick={editItem ? handleSaveEdit : handleAddItem}
-                            >
-                                {editItem ? 'Save Changes' : 'Add Item'}
-                            </Button>
-                        </Modal.Footer>
                     </Modal>
 
                     <div className="total">
